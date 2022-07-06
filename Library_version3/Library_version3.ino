a090735c53ce7dedd2248b572749e3608e77e1e3
Coleton Van Valkenburgh <99898198+ColetonV@users.noreply.github.com>
2022-07-06 12:46:48 -0400

    most recent library IMU

---
+++ Library_version3/Library_version3.ino
@@ -0,0 +1,38 @@
+/*
+  This program  reads the angles and heading from the accelerometer, gyroscope
+   and compass on a BerryIMU connected to a Teensy 3.6.
+
+  http://ozzmaker.com/
+
+*/
+#include "BerryIMU_v3.h"
+
+BerryIMU_v3 BerryIMU;
+
+void setup() {
+
+  Serial.begin(115200);  // start serial for output
+
+}
+
+void loop() {
+
+  BerryIMU.IMU_read();
+
+//  Serial.print(BerryIMU.AccXraw);
+//  Serial.print(",");
+//  Serial.print(BerryIMU.AccYraw);
+//  Serial.print(",");
+//  Serial.println(BerryIMU.AccZraw);
+//
+  Serial.print(BerryIMU.gyr_rateXraw);
+  Serial.print(",");
+  Serial.print(BerryIMU.gyr_rateYraw);
+  Serial.print(",");
+  Serial.println(BerryIMU.gyr_rateZraw);
+//
+//  Serial.println(BerryIMU.alt);
+
+  delay(10);
+
+}
