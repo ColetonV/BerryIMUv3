7aca78df2873a92a33c165b29638694673acc469
Coleton Van Valkenburgh <99898198+ColetonV@users.noreply.github.com>
2022-08-10 17:21:26 -0400

    Madgwick Filter New

---
+++ Madgwick_Filter_version7-library_trial1/Madgwick_Filter.cpp
@@ -0,0 +1,13 @@
+/*
+ Madgwick_Filter.cpp 
+*/
+
+#include "Madgwick_Filter.h "
+
+BerryIMU_v3::BerryIMU_v3(){
+
+}
+
+void BerryIMU_v3::IMU_read(){
+
+}
---
+++ Madgwick_Filter_version7-library_trial1/Madgwick_Filter.h
@@ -0,0 +1,14 @@
+/*
+ Madgwick_Filter.h 
+*/
+
+#pragma once
+#include "Arduino.h"
+
+class BerryIMU_v3
+{
+  public:
+
+  private:
+
+};
